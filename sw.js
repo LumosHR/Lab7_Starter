5ea9d8e4321d9c6a98b531259ac1adb1ca3ac05e
LumosHR <77979094+LumosHR@users.noreply.github.com>
2021-11-06 21:50:56 -0700

    finish explore

--- sw.js
+++ sw.js
@@ -2,6 +2,21 @@
 //         so do not move it next to the other scripts
 
 const CACHE_NAME = 'lab-7-starter';
+const urlsToCache = [
+  'assets/images/icons/0-star.svg',
+  'assets/images/icons/1-star.svg',
+  'assets/images/icons/2-star.svg',
+  'assets/images/icons/3-star.svg',
+  'assets/images/icons/4-star.svg',
+  'assets/images/icons/5-star.svg',
+  'assets/images/icons/arrow-down.png',
+  'https://introweb.tech/assets/json/ghostCookies.json',
+  'https://introweb.tech/assets/json/birthdayCake.json',
+  'https://introweb.tech/assets/json/chocolateChip.json',
+  'https://introweb.tech/assets/json/stuffing.json',
+  'https://introweb.tech/assets/json/turkey.json',
+  'https://introweb.tech/assets/json/pumpkinPie.json'
+];
 
 // Once the service worker has been installed, feed it some initial URLs to cache
 self.addEventListener('install', function (event) {
@@ -9,6 +24,13 @@ self.addEventListener('install', function (event) {
    * TODO - Part 2 Step 2
    * Create a function as outlined above
    */
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 /**
@@ -21,6 +43,7 @@ self.addEventListener('activate', function (event) {
    * TODO - Part 2 Step 3
    * Create a function as outlined above, it should be one line
    */
+  event.waitUntil(clients.claim());
 });
 
 // Intercept fetch requests and store them in the cache
@@ -29,4 +52,19 @@ self.addEventListener('fetch', function (event) {
    * TODO - Part 2 Step 4
    * Create a function as outlined above
    */
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          console.log('ServiceWorker respond to request: ', event.request.url);
+          return response;
+        }
+        return fetch(event.request).then(function(response) {
+          return caches.open(CACHE_NAME).then(function(cache) {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        });
+      })
+  );
 });
