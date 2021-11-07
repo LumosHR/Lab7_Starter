fe8ccc6e6aa3e1fc9ae14d67143662e0a2e130d1
LumosHR <77979094+LumosHR@users.noreply.github.com>
2021-11-06 22:18:50 -0700

    fix bugs

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,6 @@
 // main.js
 
-import { Router } from '/assets/scripts/Router.js';
+import { Router } from './Router.js';
 
 const recipes = [
   'https://introweb.tech/assets/json/ghostCookies.json',
